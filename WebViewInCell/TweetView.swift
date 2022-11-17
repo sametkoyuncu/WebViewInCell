480565cb3eaaef8c37a102b013d5ff41379a3e6d
Samet Koyuncu <sametkoyuncu@live.com>
2022-11-17 23:25:49 +0300

    Add something doesn't work

---
+++ WebViewInCell/TweetView.swift
@@ -0,0 +1,49 @@
+//
+//  TweetView.swift
+//  WebViewInCell
+//
+//  Created by Samet Koyuncu on 17.11.2022.
+//
+
+import UIKit
+import WebKit
+
+class ArticleWebView: WKWebView {
+    var isReloadCalled = false
+    var reloadRow: (()->())?
+
+  init(frame: CGRect) {
+    let configuration = WKWebViewConfiguration()
+    super.init(frame: frame, configuration: configuration)
+    self.navigationDelegate = self
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override var intrinsicContentSize: CGSize {
+      print("elma")
+    return self.scrollView.contentSize
+  }
+}
+
+extension ArticleWebView: WKNavigationDelegate {
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    webView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { (result, _) in
+        print("elma sametk \(webView.scrollView.contentSize.height) \(result)")
+        let webViewContraints = [
+            webView.heightAnchor.constraint(equalToConstant: webView.scrollView.contentSize.height)
+        ]
+        NSLayoutConstraint.activate(webViewContraints)
+        webView.invalidateIntrinsicContentSize()
+//        if self.isReloadCalled == false {
+//            self.isReloadCalled = true
+//            print("elma can")
+//            self.reloadRow?()
+//        }
+        
+        print("elma sametk")
+    })
+  }
+}
--- WebViewInCell/ViewController.swift
+++ WebViewInCell/ViewController.swift
@@ -6,31 +6,52 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
- 
+    
     @IBOutlet weak var tableView: UITableView!
-
+    
+    var webView: WKWebView!
+    var webView2: ArticleWebView!
+    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
+      //  configureWebView()
         setupTableView()
+    }
+    
+    func configureWebView() {
+        let config = WKWebViewConfiguration()
+        //let source = "document.addEventListener('click', function(){ window.webkit.messageHandlers.iosListener.postMessage('click clack!'); })"
+    //let source = "document.onreadystatechange = function(){ if(document.readyState) === 'complete'){ window.webkit.messageHandlers.iosListener.postMessage(document.body.scrollHeight)}}"
+      //  let source = "document.onreadystatechange = function(){ if(document.readyState) === 'complete'){ window.webkit.messageHandlers.iosListener.postMessage(document.body.scrollHeight)}}"
+       let source = "var interval = setInterval(function() {if(document.readyState === 'complete') { clearInterval(interval); window.webkit.messageHandlers.iosListener.postMessage(document.body.scrollHeight)}}, 100);"
+       // let source = "window.addEventListener('load', function() { window.webkit.messageHandlers.iosListener.postMessage(document.body.scrollHeight); })"
+       // let source = "window.addEventListener('load', function(){ window.webkit.messageHandlers.iosListener.postMessage(document.body.scrollHeight); })"
+        let script = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        config.userContentController.addUserScript(script)
+        config.userContentController.add(self, name: "iosListener")
+        webView = WKWebView(frame: UIScreen.main.bounds, configuration: config)
+        webView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         // for showing results
-        TweetListTableViewCell.delegate = self
         
-        tableView.isHidden = true
+        tableView.isHidden = false
         
-        tableView.register(.init(nibName: "TweetListTableViewCell", bundle: nil), forCellReuseIdentifier: TweetListTableViewCell.identifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 }
 
 // MARK: - TableView Delegate Methods
-extension ViewController: UITableViewDelegate { }
+extension ViewController: UITableViewDelegate {
+
+}
 
 // MARK: - TableView DataSource Methods
 extension ViewController: UITableViewDataSource {
@@ -39,23 +60,33 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // for using tweet index
-        // TODO: burası sıkıntılı yer 1
-        TweetListTableViewCell.rowIndex = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        webView2 = ArticleWebView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 200))
+        cell.contentView.addSubview(webView2)
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: TweetListTableViewCell.identifier, for: indexPath) as! TweetListTableViewCell
-      
+        webView2.reloadRow = {
+            self.tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        let webViewContraints = [
+            webView2.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 0),
+            webView2.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: 0),
+            webView2.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 0),
+            webView2.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: 0),
+            webView2.heightAnchor.constraint(equalToConstant: 200)
+        ]
+        NSLayoutConstraint.activate(webViewContraints)
+        
+        webView2.loadHTMLString(Data.tweets[indexPath.row], baseURL: nil)
+        
         return cell
     }
 }
 
-extension ViewController: TweetCellDelegate {
-    func showResults() {
-        tableView.reloadData()
-        tableView.isHidden = false
-        activityIndicator.stopAnimating()
-        activityIndicator.isHidden = true
+// MARK: - wk
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("message: \(message.body)")
     }
 }
-
 
